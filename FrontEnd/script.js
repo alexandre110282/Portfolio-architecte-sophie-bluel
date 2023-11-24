41aaa043022ac3460665d87c670b02ba4e6b02db
alexandre110282 <69106721+alexandre110282@users.noreply.github.com>
2023-11-24 16:57:04 +0100

    Fonctionraccourcie

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,12 +5,12 @@ const trueReponse= await reponse.json()
 console.log(trueReponse)
 console.log(reponse)
 const index= trueReponse.map(list=> list.imageUrl)
-console.log(index)
 
  
 const gallery= document.querySelector('.gallery')
 
 const boutonsText = ["Tous", "Objets", "Appartements", "Hôtels & restaurants"]
+const boutonsCat = ["Tous", "Objets", "Appartements", "Hotels & restaurants"]
 const boutonClass =[ "btn-tous", "btn-obj", "btn-appart", "btn-hotel"]
 const listFiltre= document.createElement("div")
  listFiltre.classList.add("listfiltres")
@@ -24,6 +24,7 @@ for (let i = 0; i < boutonsText.length; i++) {
     bouton.classList.add("filtres", boutonClass[i])
     // Attribution du texte au bouton à partir du tableau
     bouton.innerText = boutonsText[i]
+    bouton.setAttribute('data-category', boutonsCat[i])
     // Ajout du bouton au corps du document
     listFiltre.appendChild(bouton)
 }
@@ -34,64 +35,42 @@ portH2.parentNode.insertBefore(listFiltre, portH2.nextSibling)
 
 function genererProjets(trueReponse){
 for (let i=0; i< trueReponse.length; i++){
-    
     let figure =document.createElement("figure")
-
 //creation balise img et figcaption
     const img = document.createElement("img")
     const figcaption = document.createElement("figcaption")
-
     //insertion texte figcaption
     figcaption.innerText = trueReponse[i].title
-
     //acces image[i] et alt
     img.src = trueReponse[i].imageUrl
     img.alt = trueReponse[i].title
-
     // insertion nouveaux elements dans figure
     figure.appendChild(img)
     figure.appendChild(figcaption)
-
     //insertion figure dans balise gallery
     gallery.appendChild(figure)
-    
 }}
 genererProjets(trueReponse)
 
 
-const tous=  document.querySelector(".btn-tous")
-tous.addEventListener("click",function(){
- document.querySelector(".gallery").innerHTML =""
-    genererProjets(trueReponse)
-})
+for (let index = 0; index < boutonClass.length; index++) {
+    let choixbouton = document.querySelector("." + boutonClass[index]);
 
- const objets =  document.querySelector(".btn-obj")
-  objets.addEventListener("click", function(){
-    const objetsFiltres =  trueReponse.filter(function(objet){
-    return objet.category.name === "Objets"
-    })
-    document.querySelector(".gallery").innerHTML =""
-    genererProjets(objetsFiltres)
-  })
+    choixbouton.addEventListener("click", function () {
+        // Récupérer la catégorie à partir de l'attribut data-category
+        const category = choixbouton.getAttribute('data-category');
 
-  const apparts =  document.querySelector(".btn-appart")
-  apparts.addEventListener("click", function(){
-    const appartsFiltres =  trueReponse.filter(function(appart){
-    return appart.category.name === "Appartements"
-    })
-     document.querySelector(".gallery").innerHTML =""
-    genererProjets(appartsFiltres)
-  })
-
-  const hotels =  document.querySelector(".btn-hotel")
-  hotels.addEventListener("click", function(){
-    const hotelsFiltres =  trueReponse.filter(function(hotel){
-    return hotel.category.name === "Hotels & restaurants"
-    })
-    document.querySelector(".gallery").innerHTML =""
-    genererProjets(hotelsFiltres)
-  })
-
-  
-
-  
+        if (category === "Tous") {
+            document.querySelector(".gallery").innerHTML = "";
+            genererProjets(trueReponse);
+        } else {
+            // Filtrer les projets en fonction de la catégorie
+            const projetsFiltres = trueReponse.filter(function (projet) {
+                return projet.category.name === category;
+            });
+            // Vider la galerie et générer les projets filtrés
+            document.querySelector(".gallery").innerHTML = "";
+            genererProjets(projetsFiltres);
+        }
+    });
+}
